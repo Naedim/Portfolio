96c655dc216ca236c212eb62ac7587c4e17d3efd
Naedim <dnoeldev@gmail.com>
2022-11-30 21:22:28 +0100

    setting the layout to 100% height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="default-layout-container">
     <Toolbar />
     <slot />
   </div>
 </template>
+
+<style lang="scss">
+.default-layout-container {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
