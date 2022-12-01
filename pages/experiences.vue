d9af59057125b978219e893dd82509af1d99763a
Naedim <dnoeldev@gmail.com>
2022-12-01 23:49:58 +0100

    start of experience details

--- pages/experiences.vue
+++ pages/experiences.vue
@@ -7,6 +7,15 @@
       <h1 class="experiences-page__experience-year">
         {{ experience.begining }}
       </h1>
+
+      <div class="experience-page__experience-content">
+        <h2 class="experiences-page__experience-title">
+          {{ experience.title }}
+        </h2>
+        <p class="experiences-page__experience-content-description">
+          {{ experience.description }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -33,63 +42,17 @@ interface Experience {
 
 let experienceList: Array<Experience> = [];
 
-experienceList.push(
-  {
-    company: {
-      name: "capgemini",
-      website: "https://www.capgemini.com/",
-    },
-    contractType: "work-study program",
-    begining: "August 2021",
-    duration: 12,
-    title: "FullStack developer",
-    description: "",
-  }
-  // {
-  //   company: {
-  //     name: "Koust",
-  //     website: "http,s://www.capgemini.com/",
-  //   },
-  //   contractType: "Business internship",
-  //   begining: "August 2021",
-  //   duration: 12,
-  //   title: "Web/mobile developer",
-  //   description: "",
-  // },
-  // {
-  //   company: {
-  //     name: "Intia",
-  //     website: "http,s://www.capgemini.com/",
-  //   },
-  //   contractType: "Business internship",
-  //   begining: "August 2021",
-  //   duration: 12,
-  //   title: "Web/mobile developer",
-  //   description: "",
-  // },
-  // {
-  //   company: {
-  //     name: "Intia",
-  //     website: "http,s://www.capgemini.com/",
-  //   },
-  //   contractType: "Business internship",
-  //   begining: "August 2021",
-  //   duration: 12,
-  //   title: "Web/mobile developer",
-  //   description: "",
-  // },
-  // {
-  //   company: {
-  //     name: "Intia",
-  //     website: "http,s://www.capgemini.com/",
-  //   },
-  //   contractType: "Business internship",
-  //   begining: "August 2021",
-  //   duration: 12,
-  //   title: "Web/mobile developer",
-  //   description: "",
-  // }
-);
+experienceList.push({
+  company: {
+    name: "capgemini",
+    website: "https://www.capgemini.com/",
+  },
+  contractType: "work-study program",
+  begining: "August 2021",
+  duration: 12,
+  title: "FullStack developer",
+  description: "",
+});
 
 // let selectedExperience: Ref<Company> = ref(experienceList[0]);
 // let selectCompany = (index: number) => {
@@ -114,16 +77,56 @@ $fourth-color: #6b7dad;
   justify-content: center;
 }
 .experience-page__experience {
-  height: 50%;
+  height: 80%;
   width: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
 
   h1.experiences-page__experience-year {
     width: 100%;
+    height: 15%;
     text-align: center;
-    font-size: clamp(1.25rem, 3vw, 2.5rem);
+    font-size: clamp(1.5rem, 3vw, 2.5rem);
     color: white;
     font-weight: 500;
   }
+
+  .experience-page__experience-content {
+    margin-top: 2%;
+
+    box-sizing: border-box;
+    width: 90%;
+    border-color: $secondary-color;
+    border-style: solid;
+    border-width: 3px;
+    border-radius: 40px;
+
+    padding: 4%;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    h2.experiences-page__experience-title {
+      color: $secondary-color;
+      flex-grow: 1;
+      font-size: clamp(1.25rem, 3vw, 2.5rem);
+      font-weight: 500;
+    }
+  }
 }
 </style>
+
+// { // company: { // name: "Koust", // website: "http,s://www.capgemini.com/",
+// }, // contractType: "Business internship", // begining: "August 2021", //
+duration: 12, // title: "Web/mobile developer", // description: "", // }, // {
+// company: { // name: "Intia", // website: "http,s://www.capgemini.com/", // },
+// contractType: "Business internship", // begining: "August 2021", // duration:
+12, // title: "Web/mobile developer", // description: "", // }, // { // company:
+{ // name: "Intia", // website: "http,s://www.capgemini.com/", // }, //
+contractType: "Business internship", // begining: "August 2021", // duration:
+12, // title: "Web/mobile developer", // description: "", // }, // { // company:
+{ // name: "Intia", // website: "http,s://www.capgemini.com/", // }, //
+contractType: "Business internship", // begining: "August 2021", // duration:
+12, // title: "Web/mobile developer", // description: "", // }
