80a82a18df16be841ee3408e3bb826e8ef6e9655
Naedim <dnoeldev@gmail.com>
2022-12-04 20:56:56 +0100

    start of experience info

--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,38 +1,45 @@
 <template>
   <div class="experiences-page">
-    <div class="experience-page__experience" v-for="experience of experienceList">
+    <div
+      class="experience-page__experience"
+      v-for="experience of experienceList"
+    >
       <div class="experiences-page__selected-experience">
-        <h1>
+        <h2>
           {{ experience.begining }}
-        </h1>
-
-        <div class="experiences-page__company-info">
-          <h2 class="experience-page__company-name">{{ experience.company.name }}</h2>
-
-          <div class="experience-page__experience-duration">
-            <Icon icon="material-symbols:hourglass-bottom" class="hourglass-icon" title="navbar's logo's icon" />
-
-            <h2>{{ experience.duration }} months</h2>
-
-          </div>
-
-        </div>
-
+        </h2>
       </div>
       <div class="experience-page__experience-content">
-        <h2 class="experiences-page__experience-title">
-          {{ experience.title }}
-        </h2>
-        <p class="experiences-page__experience-content-description">
-          {{ experience.description }}
-        </p>
+        <div class="experience-page__experience-company">
+          <a
+            :href="experience.company.website"
+            target="_blank"
+            rel="noopener noreferrer"
+            alt="company's name and link to the company website"
+          >
+            <h2>
+              {{ experience.company.name }}
+            </h2>
+            <Icon icon="mdi:open-in-new" title="open in new tab" />
+          </a>
+        </div>
+
+        <div class="experience-page__experience-title">
+          <h2>
+            {{ experience.title }}
+          </h2>
+        </div>
+
+        <div class="experience-page__experience-duration">
+          <h3>{{ experience.duration }} mounths</h3>
+          <Icon icon="ic:baseline-hourglass-bottom" title="open in new tab" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Ref } from "vue";
 import { Icon } from "@iconify/vue";
 /**
  * Informations about professionnal experiences, duration in months
@@ -42,6 +49,11 @@ interface Company {
   name: string;
   website: string;
 }
+
+interface Skillset {
+  description: string;
+  skillsLogos: string[];
+}
 interface Experience {
   company: Company;
   contractType: "work-study program" | "Business internship";
@@ -49,13 +61,14 @@ interface Experience {
   duration: number;
   title: string;
   description: string;
+  Skillsetlist: Skillset[];
 }
 
 let experienceList: Array<Experience> = [];
 
 experienceList.push({
   company: {
-    name: "capgemini",
+    name: "Capgemini",
     website: "https://www.capgemini.com/",
   },
   contractType: "work-study program",
@@ -63,17 +76,14 @@ experienceList.push({
   duration: 12,
   title: "FullStack developer",
   description: "",
+
+  Skillsetlist: [
+    {
+      description: "client development",
+      skillsLogos: ["vue", "react", "angular"],
+    },
+  ],
 });
-
-// let selectedExperience: Ref<Company> = ref(experienceList[0]);
-// let selectCompany = (index: number) => {
-//   console.log(index);
-//   selectedCompany.value = companies[index];
-
-//   console.log(selectedCompany);
-// };
-
-// console.log(companies);
 </script>
 <style scoped lang="scss">
 $secondary-color: #d81264;
@@ -97,72 +107,81 @@ $fourth-color: #6b7dad;
 
   .experiences-page__selected-experience {
     width: 100%;
-    height: 30%;
+    height: 10%;
 
-    h1 {
+    h2 {
       text-align: center;
       font-size: clamp(2rem, 3vw, 2.5rem);
       color: white;
       font-weight: 500;
     }
-
-    .experiences-page__company-info {
-      display: flex;
-      flex-direction: column;
-
-      align-items: center;
-      justify-content: center;
-
-        h2.experience-page__company-name {
-          font-size: clamp(2.5rem, 2.5vw, 2rem);
-          color: white;
-          font-weight: 500;
-        }
-
-
-
-      .experience-page__experience-duration {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        h2{
-          text-align: center;
-        font-size: clamp(1.25rem, 3vw, 2.5rem);
-        color : $third-color;
-        font-weight: 500;
-        }
-
-        .hourglass-icon {
-          color: $fourth-color;
-          width: 1.5rem;
-          font-size: clamp(1.25rem, 3vw, 2.5rem);
-        }
-      }
-    }
   }
 
   .experience-page__experience-content {
     margin-top: 2%;
-
     box-sizing: border-box;
     width: 90%;
+    padding-left: 4%;
+    padding-right: 4%;
+
     border-color: $secondary-color;
     border-style: solid;
     border-width: 3px;
-    border-radius: 40px;
-
-    padding: 4%;
+    border-radius: 10px;
     flex-grow: 1;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
 
-    h2.experiences-page__experience-title {
-      color: $secondary-color;
-      flex-grow: 1;
-      font-size: clamp(1.25rem, 3vw, 2.5rem);
-      font-weight: 500;
+    display: flex;
+    flex-direction: column;
+    .experience-page__experience-company {
+      display: flex;
+      justify-content: center;
+      a {
+        display: flex;
+        font-size: clamp(1.5rem, 3vw, 2.5rem);
+        align-items: center;
+        color: $third-color;
+        display: flex;
+        gap: 5px;
+
+        h2 {
+          color: $third-color;
+          font-weight: 500;
+        }
+
+        svg {
+          font-size: clamp(1.5rem, 3vw, 2.5rem);
+        }
+
+        &:hover {
+          h2,
+          svg {
+            color: $secondary-color;
+          }
+        }
+      }
+    }
+
+    .experience-page__experience-title {
+      h2 {
+        font-size: clamp(0.8rem, 3vw, 2.5rem);
+        color: $third-color;
+        font-weight: 500;
+      }
+    }
+
+    .experience-page__experience-duration {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      h3 {
+        font-size: clamp(0.8rem, 3vw, 2.5rem);
+        color: $third-color;
+        font-weight: 500;
+      }
+      svg {
+        color: $third-color;
+        font-size: clamp(0.8rem, 3vw, 2.5rem);
+      }
     }
   }
 }
