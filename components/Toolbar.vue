1f132927879604f1036b80a5bae5b565c20f837b
Syhun <93095067+Syhun@users.noreply.github.com>
2023-03-25 20:41:05 +0100

    toolbar tailwind done

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,36 +1,71 @@
-<template class = "@container">
+<template class="@container">
   <nav>
-
-    <div class="w-full flex justify-between items-center text-[2.5vh] text-white">
-      <NuxtLink id="toolbar__navbar-logo" class="flex items-center gap-[2vw] slate-400 font-semibold" to="#"
-        title="navbar's logo">
+    <div
+      class="flex w-full items-center justify-between text-[2.5vh] text-white"
+    >
+      <NuxtLink
+        id="toolbar__navbar-logo"
+        class="slate-400 flex items-center gap-[2vw] font-semibold"
+        to="#"
+        title="navbar's logo"
+      >
         Damien NOEL
-        <Icon icon="material-symbols:code" class="toolbar__navbar-log-icon" title="navbar's logo's icon" />
+        <Icon
+          icon="material-symbols:code"
+          class="toolbar__navbar-log-icon"
+          title="navbar's logo's icon"
+        />
       </NuxtLink>
-
-      <ul id="mobile-menu" class= "md:hidden absolute top-0 left-0 w-full h-full backdrop-blur ml-[-100%] 
-                      transition-all duration-200 pt-[20vh] box-border sm:flex flex-col items-center text-[3vh] gap-[5vh] {'ml-0' isMobileMenuActive }" >
+      <ul
+        id="toolbar__mobile-menu"
+        :class="isMobileMenuActive ? 'ml-0' : 'ml-[-100%]'"
+        class="absolute top-0 left-0 box-border h-full w-full flex-col items-center gap-[5vh] pt-[20vh] text-[3vh] backdrop-blur transition-all duration-200 sm:flex md:hidden"
+      >
         <li v-for="page of pages">
-          <NuxtLink :to="page" class="text-white hover:lime-400 focus:lime-400" @click="menuToggle()">{{ page }}
+          <NuxtLink
+            :to="page"
+            class="hover:lime-400 focus:lime-400 text-white"
+            @click="menuToggle()"
+            >{{ page }}
           </NuxtLink>
         </li>
-
       </ul>
 
-      <Icon icon="mdi:menu" class="text-4vh w-auto text-white z-10]" title="hamburger menu"
-        v-on:click="() => menuToggle()" />
+      <ul
+        id="toolbar__big-screen-menu"
+        class="hidden grow items-center justify-evenly transition-all duration-200 md:flex"
+      >
+        <li v-for="page of pages">
+          <NuxtLink :to="page" class="hover:lime-400 focus:lime-400 text-white"
+            >{{ page }}
+          </NuxtLink>
+        </li>
+      </ul>
+
+      <button
+        class="z-10 w-auto text-[4vh] text-white md:hidden"
+        v-on:click="() => menuToggle()"
+      >
+        <Icon icon="mdi:menu" title="hamburger menu" />
+      </button>
     </div>
 
-    <div id = "toolbar__current-page-container" class="flex items-center gap-2 text-purple-600 text-[2vh]">
-      <Icon icon="mdi:book-open-page-variant-outline" title="navbar's logo's icon" />
+    <div
+      id="toolbar__current-page-info"
+      class="flex items-center gap-2 text-[2vh] text-purple-700"
+    >
+      <Icon
+        icon="mdi:book-open-page-variant-outline"
+        title="navbar's logo's icon"
+        class=""
+      />
 
-      <h1 id="current-page-name" title="current page's name">
+      <h1 id="toolbar__current-page-name" title="current page's name">
         {{ route.name }}
       </h1>
     </div>
   </nav>
 </template>
-
 
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
@@ -39,139 +74,8 @@ const pages = ["home", "experiences", "education", "about"];
 
 let isMobileMenuActive = ref(false);
 
-
 function menuToggle(): void {
+  console.log("menuToggle");
   isMobileMenuActive.value = !isMobileMenuActive.value;
-};
+}
 </script>
-
-
-<style scoped lang="scss">
-.toolbar__navbar {
-  //   width: 100%;
-  //   display: flex;
-  //   justify-content: space-between;
-  //   align-items: center;
-
-  //   font-size: 2.5vh;
-
-  .toolbar__navbar-logo {
-    //   display: flex;
-    //   align-items: center;
-    //   gap: 2vw;
-    //   color: $slate-4;
-    //   font-weight: 600;
-
-    svg {
-      // font-size: 3vh;
-
-    }
-
-  }
-
-  a {
-    // color: $secondary;
-
-    &:hover {
-      // color: $accent;
-    }
-
-    &:focus {
-      // color: $accent;
-    }
-  }
-
-
-  .menu-hamburger {
-    // font-size: 4vh;
-    // width: auto;
-    // color: $secondary;
-    // z-index: 1;
-  }
-}
-
-div.toolbar__current-page-container {
-  // display: flex;
-  // align-items: center;
-  // gap: 2%;
-
-  .page-icon {
-    // color: $accent;
-    // margin-left: 0;
-    // font-size: 2vh;
-  }
-
-  .current-page-name {
-    // font-size: 2vh;
-    // color: $accent;
-  }
-}
-
-//concerns the state of the menu bar for mobiles
-@media screen and (max-width: 1000px) {
-
-  .nav-links {
-    // position: absolute;
-    // top: 0;
-    // left: 0;
-    // width: 100vw;
-    // height: 100vh;
-    // backdrop-filter: blur(9px);
-    // margin-left: -100%;
-    // transition: all 0.2s ease;
-
-    // padding-top: 20vh;
-
-    // box-sizing: border-box;
-    // display: flex;
-    // flex-direction: column;
-    // align-items: center;
-
-    // font-size: 3vh;
-    // gap: 5vh;
-
-
-    &.mobile-menu {
-      // margin-left: 0;
-    }
-  }
-}
-
-//higher then mobile
-@media screen and (min-width: 1000px) {
-  .toolbar__navbar {
-    padding-top: 2vh;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    .toolbar__navbar-logo {
-      font-size: 2.5vw;
-    }
-  }
-
-  .current-page-indicator {
-    display: none;
-  }
-
-  .nav-links {
-    // width: 60vw;
-    // display: flex;
-    // justify-content: space-evenly;
-    // font-weight: 500;
-    // font-size: 2.5vw;
-
-    // a{
-    //   font-size: 2vw;
-    // }
-  }
-
-  .menu-hamburger {
-    display: none;
-  }
-
-  div.toolbar__current-page-container {
-    display: none;
-  }
-}
-</style>
