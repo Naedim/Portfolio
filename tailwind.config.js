2f0d93349aabdc6f6b8068249970b868e4578f9f
Syhun <93095067+Syhun@users.noreply.github.com>
2023-03-25 20:41:05 +0100

    toolbar tailwind done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors :{
-    },
     screens: {
       'sm': '280px',
 
