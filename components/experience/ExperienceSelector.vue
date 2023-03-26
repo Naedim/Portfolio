103ee415520dfb60e2761e5c7994931ab1d81562
Naedim <93095067+Syhun@users.noreply.github.com>
2023-03-26 22:26:49 +0200

    start of experience selector component

---
+++ components/experience/ExperienceSelector.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    id="experience-page__selector"
+    class="mt-[3vh] grid w-full grid-flow-row grid-cols-6 grid-rows-2 items-center text-center"
+  >
+    <button
+      id="reverse"
+      class="selector-button row-span-1 row-start-1 rotate-180"
+      title="next experience"
+      @click="previousExperience"
+      :class="{ hidden: currentExperienceIndex === 0 }"
+    >
+      <Icon icon="ic:baseline-arrow-forward-ios" />
+    </button>
+    <h2 class="col-span-4 col-start-2 row-start-1 text-[8vw] text-white">
+      {{ selectedExperience.company.name }}
+    </h2>
+
+    <button
+      title="next experience"
+      @click="nextExperience"
+      :class="{
+        hidden: currentExperienceIndex === experienceList.length - 1,
+      }"
+      class="selector-button col-start-6 row-start-1"
+    >
+      <Icon icon="ic:baseline-arrow-forward-ios" />
+    </button>
+
+    <p
+      class="col-span-4 col-start-2 row-start-2 text-[5vw] font-semibold text-red-300"
+    >
+      {{ selectedExperience.begining }} - {{ selectedExperience.duration }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import Experience2 from "~~/components/experience/Experience2.vue";
+import { Ref } from "vue";
+import { ExperienceInterface } from "~~/models/interfaces/ExperienceInteface";
+
+const props = defineProps<{
+  experienceList: Array<ExperienceInterface>;
+}>();
+
+let currentExperienceIndex = 0;
+
+let selectedExperience: Ref<ExperienceInterface> = ref(
+  props.experienceList[currentExperienceIndex]
+);
+
+
+function nextExperience() {
+  console.log("next");
+  selectExperience(currentExperienceIndex + 1);
+}
+function previousExperience() {
+  selectExperience(currentExperienceIndex - 1);
+}
+
+function selectExperience(index: number): void {
+  currentExperienceIndex = index;
+  selectedExperience.value = props.experienceList[index];
+  this.$emit('created', index);  
+}
+</script>
+
+<style scoped>
+.selector-button {
+  @apply flex items-center justify-center text-[9vw] text-red-500 hover:text-orange-200;
+}
+</style>
--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,78 +1,40 @@
 <template>
-  <div id="experience-page" class="">
-    <div
-      id="experience-page__selector"
-      class="grid w-[90%] grid-flow-row grid-cols-6 grid-rows-3 items-center text-center"
-    >
-      <button
-        id="reverse"
-        class="selector-button row-span-1 row-start-2 rotate-180"
-        title="next experience"
-        @click="previousExperience"
-        :class="{ '': currentExperienceIndex === 0 }"
-      >
-        <Icon icon="ic:baseline-arrow-forward-ios" />
-      </button>
-      <h2 class="col-span-4 col-start-2 row-start-2 text-[6vw] text-white">
-        {{ selectedExperience.company.name }}
-      </h2>
+  <div id="experience-page" class="flex flex-col items-center">
+    
+    <!-- <Experience2 :experience="selectedExperience" /> -->
+    <ExperienceSelector :experienceList="experienceList"/>
 
-      <button
-        title="next experience"
-        @click="nextExperience"
-        :class="{ '': currentExperienceIndex === experienceList.length - 1 }"
-        class="selector-button col-start-6 row-start-2"
-      >
-        <Icon icon="ic:baseline-arrow-forward-ios" />
-      </button>
-      <p
-        class="col-span-4 col-start-2 row-start-3 text-[3vw] font-semibold text-red-300"
-      >
-        {{ selectedExperience.begining }} - {{ selectedExperience.duration }}
-      </p>
-    </div>
+    {{ experienceList[experienceIndex].begining}}
 
-    <Experience2 :experience="selectedExperience" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ExperienceInterface } from "@/models/interfaces/ExperienceInteface";
-import Experience2 from "~~/components/experience/Experience2.vue";
-import { Icon } from "@iconify/vue";
+
 import { Ref } from "vue";
-import experiences from "../assets/staticData/experiences.json";
+import { ExperienceInterface } from "~~/models/interfaces/ExperienceInteface";
+import experiencesData from "../assets/staticData/experiences.json";
+const experienceList: Array<ExperienceInterface> = experiencesData as Array<ExperienceInterface>;
+
+let experienceIndex : Ref<number> = ref(0);
+
+function updateExperience(index : number) {
+  console.log("updateExperience");
+  experienceIndex.value++;
+}
+
+
+
 
 /**
  * Informations about professionnal experiences, duration in months
  */
 
-let experienceList: Array<ExperienceInterface> =
-  experiences as Array<ExperienceInterface>;
 
-let selectedExperience: Ref<ExperienceInterface> = ref(experienceList[0]);
-let currentExperienceIndex = 0;
 
-function nextExperience() {
-  console.log("next");
-  selectExperience(currentExperienceIndex + 1);
-}
-function previousExperience() {
-  selectExperience(currentExperienceIndex - 1);
-}
-
-function selectExperience(index: number): void {
-  console.log("selectExperience with ", index);
-  selectedExperience.value = experienceList[index];
-  currentExperienceIndex = index;
-  console.log("new index : " + currentExperienceIndex);
-  console.log("experience list : " + experienceList.length);
-}
 </script>
 <style scoped>
-.selector-button {
-  @apply flex items-center justify-center text-[6vw] text-red-500 hover:text-orange-200;
-}
+
 </style>
 
 // { // company: { // name: "Koust", // website: "http,s://www.capgemini.com/",
