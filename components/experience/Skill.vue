49863452cc5116f32bd71d129d3747728776fda5
Damien NOEL <dnoeldev@gmail.com>
2023-04-10 00:34:56 +0200

    fix for assets not being charged on remote server

--- components/experience/Skill.vue
+++ components/experience/Skill.vue
@@ -3,15 +3,18 @@
     class="min-w-20 sm:min-w-25 md:min-w-30 lg:max-w-[15rem] p-y-1 box-border flex h-6 grow items-center justify-center gap-2 rounded-full 
     bg-slate-200 p-1 sm:gap-3 sm:p-2 md:h-14 md:gap-4 md:p-3 lg:h-12"
   >
-    <img :title="skill.name" class="h-full" :src="`/assets/skillsPictures/${props.skill.logo}`" />
+    <img :title="skill.name" class="h-full" :src="logoPath" />
     <h5 class="text-xs font-semibold text-neutral-700 sm:text-sm md:text-2xl">{{ skill.name }}</h5>
   </button>
 </template>
 
 <script setup lang="ts">
 import {SkillInterface} from "~~/models/interfaces/SkillInterface";
-
 const props = defineProps<{
   skill: SkillInterface;
 }>();
+
+const logoPath = `/assets/skillsPictures/${props.skill.logo}`;
+
+
 </script>
