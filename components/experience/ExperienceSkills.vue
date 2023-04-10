d66e55150b31f908db340b4f09e3c591f84ad3c1
Damien NOEL <dnoeldev@gmail.com>
2023-04-10 22:09:53 +0200

    addition of more experiences + responsive css

--- components/experience/ExperienceSkills.vue
+++ components/experience/ExperienceSkills.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex h-2/5 lg:h-1/5 lg:w-2/3 flex-col">
-    <div class="flex flex-wrap gap-2  md:gap-4 lg:gap-8">
+    <div class="flex flex-wrap gap-2  justify-center md:gap-4 lg:gap-8">
       <button
         :title = "`domain of ${domain.title}`"
         v-for="(domain, index) in selectedExperience.skillDomainList"
         @click=" selectedDomainIndex= index"
         :class=" index === selectedDomainIndex ? 'bg-orange-200 text-neutral-700' : 'bg-red-700 text-white'"
-        class=" box-content h-6 sm:h-8 md:h-16 grow rounded-full px-2 sm:px-3 md:px-4 focus:bg-orange-200"
+        class=" box-content h-6 sm:h-8 md:h-16 grow rounded-full px-2 sm:px-3 md:px-4 focus:bg-orange-200 lg:max-w-[60rem]"
       >
         <p class="text-center text-xs sm:text-base md:text-xl font-bold">{{ domain.title }}</p>
       </button>
     </div>
-    <div class="flex flex-wrap items-start justify-center gap-3 md:gap-10 lg:gap-5 self-start w-full rounded-lg pt-10 sm:pt-15 md:pt-20">
-      <Skill class = "lg:max-w-sm " v-for="skill in  selectedExperience.skillDomainList[selectedDomainIndex].skillList" :skill="skill" />
+    <div class="flex flex-wrap items-center justify-center gap-3 lg:gap-5  w-full rounded-lg pt-10 sm:pt-15 md:pt-15">
+      <Skill v-for="skill in  selectedExperience.skillDomainList[selectedDomainIndex].skillList" :skill="skill" />
     </div>
   </div>
 </template>
--- components/experience/Skill.vue
+++ components/experience/Skill.vue
@@ -1,14 +1,15 @@
 <template>
-  <button 
-      :title="`what is ${props.skill.name}`" class = " grow  duration-500">
+  <button
+    :title="`what is ${props.skill.name}`"
+    class=" h-8 box-border min-w-[4rem] grow p-1 sm:h-12 sm:p-2 md:h-20 md:max-w-[30rem] md:min-w-[18rem] md:p-3 lg:h-13 lg:min-w-[15rem] lg:max-w-[20rem]"
+  >
     <NuxtLink
       :to="skillInfos[props.skill.name].website"
       target="_blank"
-      class="p-y-1 box-border flex h-6 min-w-[4rem] items-center justify-center gap-2 rounded-full bg-slate-200 p-1 sm:h-10 sm:gap-3 sm:p-2 
-      md:h-14 md:max-w-[30rem] md:gap-4 md:p-3 lg:h-12 lg:min-w-[15rem] lg:max-w-[20rem]"
+      class="flex items-center justify-center gap-2 h-full rounded-full bg-slate-200 md:gap-4 sm:gap-x-3"
       :title="`what is ${props.skill.name}`"
     >
-      <img :title="skill.name" class="h-full" :src="`/skillsPictures/${skillInfos[props.skill.name].logo}`" />
+      <img :title="skill.name" class="h-4 sm:h-6 md:h-9" :src="`/skillsPictures/${skillInfos[props.skill.name].logo}`" />
       <h5 class="text-xs font-semibold text-neutral-700 sm:text-sm md:text-2xl">{{ skill.name }}</h5>
     </NuxtLink>
   </button>
