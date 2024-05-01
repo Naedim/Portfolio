45d4d4278935bc0d4059a6cbb772fa0d03696c08
Syhun <93095067+Syhun@users.noreply.github.com>
2024-05-01 21:19:47 +0200

    #dno fixed height for the experience description

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -6,7 +6,9 @@
         class="flex items-start gap-2 text-violet-900 font-semibold text-base lg:text-xl xl:text-2xl"
         title="navbar's logo"
       >
-        Damien NOEL
+      <div class = "w-full h-full bg-red-500">
+        
+      </div>
       </div>
 
       <div
